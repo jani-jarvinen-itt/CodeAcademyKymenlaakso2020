6b7ed13d3768b9dc919c7ed4d65df3cbb685d6a4
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-10-06 14:39:16 +0300

    Lisätty CTS-tekniikkaa demostroiva C#- ja Visual Basic -sovellus.

---
+++ DotNet/CommonTypeSystemDemo/CtsTestApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CtsTestApp
+{
+    public class SuperLaskin : CommonTypeSystemDemo.Laskin
+    {
+        public int Tulo(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aloitetaan CTS-testaus.");
+
+            CommonTypeSystemDemo.Laskin laskin = new CommonTypeSystemDemo.Laskin();
+            int summa = laskin.Summa(123, 456);
+
+            Console.WriteLine("Summa on: " + summa);
+        }
+    }
+}
