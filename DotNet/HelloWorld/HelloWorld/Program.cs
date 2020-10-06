4178b6ee82a9dfa53c93a41bb85f3ae61dd165e1
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-10-06 11:08:05 +0300

    Lisätty hiukan C#-sovelluslogiikkaa.

--- DotNet/HelloWorld/HelloWorld/Program.cs
+++ DotNet/HelloWorld/HelloWorld/Program.cs
@@ -11,6 +11,17 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Moi Code Academy Kymenlaakso!");
+
+            Random random = new Random();
+            int maxValue = random.Next(10, 20);
+
+            for (int i = 0; i < maxValue; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine($"Luku {i} on kolmella jaollinen.");
+                }
+            }
         }
     }
 }
