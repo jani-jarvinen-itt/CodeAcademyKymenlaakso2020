eabe2c170732eefc227763967c2df4226d61c20d
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-10-06 11:13:46 +0300

    Lisätty Visual Basic -esimerkki CTS-tekniikasta.

---
+++ DotNet/CommonTypeSystemDemo/CommonTypeSystemDemo/Laskin.vb
@@ -0,0 +1,16 @@
+﻿
+Public Class Laskin
+
+    Public Function Summa(ByVal a As Integer, ByVal b As Integer) As Integer
+
+        Return a + b
+
+    End Function
+
+    Public Function Miinus(ByVal a As Integer, ByVal b As Integer) As Integer
+
+        Return a - b
+
+    End Function
+
+End Class
