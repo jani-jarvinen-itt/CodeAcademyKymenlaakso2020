39263b2a58983a1ab6a7dcf01f6a9d5624b18147
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-10-06 14:50:21 +0300

    Lisätty FileStream-luokkaa käyttävä lukujen keskiarvon laskentasovellus.

---
+++ DotNet/FileStreamDemo/FileStreamDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileStreamDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tiedostoNimi = @"C:\Academy\Kotka\Git\CodeAcademyKymenlaakso2020\DotNet\Lukuja.txt";
+            FileStream stream = new FileStream(tiedostoNimi, FileMode.Open, FileAccess.Read);
+
+            byte[] tavut = new byte[stream.Length];
+            stream.Read(tavut, 0, (int)stream.Length);
+            string teksti = Encoding.ASCII.GetString(tavut);
+            string[] rivit = teksti.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            List<int> luvut = new List<int>();
+            foreach (string rivi in rivit)
+            {
+                int luku = int.Parse(rivi);
+                luvut.Add(luku);
+            }
+
+            double keskiarvo = luvut.Average();
+            Console.WriteLine("Keskiarvo on: " + keskiarvo);
+        }
+    }
+}
