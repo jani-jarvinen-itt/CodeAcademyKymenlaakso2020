173ef512c64830801025f83d1a303116b8cb07f7
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-10-07 11:52:06 +0300

    Yksinkertainen esimerkki virheenhallinnasta. Huom! Tarkoituksellisesti virheellinen tiedostopolku.

--- DotNet/TiedostoJaPoikkeukset/TiedostoJaPoikkeukset/Program.cs
+++ DotNet/TiedostoJaPoikkeukset/TiedostoJaPoikkeukset/Program.cs
@@ -11,18 +11,25 @@ namespace TiedostoJaPoikkeukset
     {
         static void Main(string[] args)
         {
-            string tiedostoNimi = @"C:\Academy\Kotka\Git\CodeAcademyKymenlaakso2020\DotNet\Lukuja.txt";
-            string[] rivit = File.ReadAllLines(tiedostoNimi);
-
-            List<int> luvut = new List<int>();
-            foreach (string rivi in rivit)
+            try
             {
-                int luku = int.Parse(rivi);
-                luvut.Add(luku);
-            }
+                string tiedostoNimi = @"C:\X-Academy\Kotka\Git\CodeAcademyKymenlaakso2020\DotNet\Lukuja.txt";
+                string[] rivit = File.ReadAllLines(tiedostoNimi);
 
-            double keskiarvo = luvut.Average();
-            Console.WriteLine("Keskiarvo on: " + keskiarvo);
+                List<int> luvut = new List<int>();
+                foreach (string rivi in rivit)
+                {
+                    int luku = int.Parse(rivi);
+                    luvut.Add(luku);
+                }
+
+                double keskiarvo = luvut.Average();
+                Console.WriteLine("Keskiarvo on: " + keskiarvo);
+            }
+            catch
+            {
+                Console.WriteLine("Tiedoston lukemisessa tapahtui virhe.");
+            }
         }
     }
 }
