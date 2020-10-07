cf393742a7e6efa685a31814b094b461d2374287
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-10-07 14:39:15 +0300

    Lisätty HTTP-datan lataaminen ja albumien nimien tulostaminen. Lisätty myös Newtonsoftin NuGet-paketti osaksi projektia.

---
+++ DotNet/HttpClientDemo/HttpClientDemo/Album.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientDemo
+{
+    public class Album
+    {
+        public int userId { get; set; }
+
+        public int id { get; set; }
+
+        public string title { get; set; }
+    }
+}
--- DotNet/HttpClientDemo/HttpClientDemo/Program.cs
+++ DotNet/HttpClientDemo/HttpClientDemo/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,34 @@ namespace HttpClientDemo
     {
         static void Main(string[] args)
         {
+            // alustukset
+            string url = "http://jsonplaceholder.typicode.com/albums";
+            HttpClient client = new HttpClient();
+
+            // tehdään HTTP-pyyntö
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            string json = response.Content.ReadAsStringAsync().Result;
+            // Console.WriteLine(json);
+
+            // muunnetaan JSON-data olioksi
+            List<Album> albumit = JsonConvert.DeserializeObject<List<Album>>(json);
+
+            // lajitellaan nimet
+            List<string> nimet = new List<string>();
+            foreach (Album albumi in albumit)
+            {
+                nimet.Add(albumi.title);
+            }
+            nimet.Sort();
+
+            // tulostus
+            foreach (string nimi in nimet)
+            {
+                Console.WriteLine(nimi);
+            }
+
+            // client.Dispose();
+            Console.ReadLine();
         }
     }
 }
