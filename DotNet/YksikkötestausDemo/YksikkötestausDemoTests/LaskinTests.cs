9d2e6e5bd5ba2c8134745d565c18c572eca0686e
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-10-13 15:43:04 +0300

    Lisätty C#-kielinen yksikkötestiesimerkki.

---
+++ DotNet/YksikkötestausDemo/YksikkötestausDemoTests/LaskinTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YksikkötestausDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YksikkötestausDemo.Tests
+{
+    [TestClass()]
+    public class LaskinTests
+    {
+        [TestMethod()]
+        public void SummaTest()
+        {
+            Laskin laskin = new Laskin();
+            int a = 123;
+            int b = 321;
+
+            int summa = laskin.Summa(a, b);
+
+            // tarkistus
+            int odotettu = a + b;
+
+            Assert.AreEqual(odotettu, summa);
+        }
+    }
+}
