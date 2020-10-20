98ca038581bc9b7c26e97bd27938473f655c7bc8
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-10-20 13:54:54 +0300

    Lisätty LINQ-kysely numeroiden hakemiseen ja tulostamiseen.

--- DotNet/LinqDemo/LinqDemo/MainWindow.xaml.cs
+++ DotNet/LinqDemo/LinqDemo/MainWindow.xaml.cs
@@ -28,6 +28,19 @@ namespace LinqDemo
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             int[] luvut = { 4, 8, 3, 9, 1, 10, 7, 2, 5 };
+            List<int> tulokset = (from l in luvut
+                                  where l > 5
+                                  orderby l
+                                  select l).ToList();
+
+            // MessageBox.Show("Tyyppi: " + tulokset.GetType().FullName);
+            foreach (int luku in tulokset)
+            {
+                MessageBox.Show(luku.ToString());
+            }
+
+            /*
+            int[] luvut = { 4, 8, 3, 9, 1, 10, 7, 2, 5 };
             List<int> tulokset = new List<int>();
             foreach (int luku in luvut)
             {
@@ -43,6 +56,7 @@ namespace LinqDemo
             {
                 MessageBox.Show(luku.ToString());
             }
+            */
         }
     }
 }
