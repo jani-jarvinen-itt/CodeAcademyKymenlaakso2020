f2de9dc12cfa0cbb927e9c7ad7c667a7b57fff2b
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-10-20 13:31:40 +0300

    Aloitettu LINQ-esimerkkisovelluksen kehittäminen. Lisätty taulukon haku ja lajittelu peruskoodina.

---
+++ DotNet/LinqDemo/LinqDemo/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LinqDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int[] luvut = { 4, 8, 3, 9, 1, 10, 7, 2, 5 };
+            List<int> tulokset = new List<int>();
+            foreach (int luku in luvut)
+            {
+                if (luku > 5)
+                {
+                    tulokset.Add(luku);
+                }
+            }
+
+            tulokset.Sort();
+
+            foreach (int luku in tulokset)
+            {
+                MessageBox.Show(luku.ToString());
+            }
+        }
+    }
+}
