c02a71becabec100d624770e15b9352aa6205847
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-11-03 11:26:43 +0200

    Lisätty esimerkki oman olion käytöstä AP-kontrollerissa.

--- DotNet/AspNetWebApiDemo/AspNetWebApiDemo/Controllers/TestController.cs
+++ DotNet/AspNetWebApiDemo/AspNetWebApiDemo/Controllers/TestController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetWebApiDemo.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,6 +12,14 @@ namespace AspNetWebApiDemo.Controllers
     [ApiController]
     public class TestController : ControllerBase
     {
-        
+        public Asiakas Moi()
+        {
+            return new Asiakas()
+            {
+                AsiakasId = 1001,
+                Nimi = "Matti Möttönen",
+                Puhelinnumero = "09 123 4567"
+            };
+        }
     }
 }
---
+++ DotNet/AspNetWebApiDemo/AspNetWebApiDemo/Models/Asiakas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetWebApiDemo.Models
+{
+    public class Asiakas
+    {
+        public int AsiakasId { get; set; }
+
+        public string Nimi { get; set; }
+
+        public string Puhelinnumero { get; set; }
+    }
+}
