a03afae93800eb1e7471fc542da7fa17413b9ce7
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-11-03 15:19:27 +0200

    Lisätty Customers-kontrolleri sekä siihen Read-toiminnot.

---
+++ DotNet/AspNetWebApiDemo/AspNetWebApiDemo/Controllers/CustomersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetWebApiDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetWebApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        // vastaa osoitteessa: /api/customers
+        public List<Customers> PalautaKaikki()
+        {
+            NorthwindContext konteksti = new NorthwindContext();
+            List<Customers> kaikki = konteksti.Customers.ToList();
+
+            return kaikki;
+        }
+
+        // vastaa osoitteessa: /api/customers/NIMI
+        [Route("{customerId}")]
+        public Customers PalautaYksittäinen(string customerId)
+        {
+            NorthwindContext konteksti = new NorthwindContext();
+            Customers asiakas = konteksti.Customers.Find(customerId);
+
+            return asiakas;
+        }
+    }
+}
