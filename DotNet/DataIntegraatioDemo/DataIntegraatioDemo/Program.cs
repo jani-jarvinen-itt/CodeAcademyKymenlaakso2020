33e6bff3dd61392afcc651058a0cd1412444a99d
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-11-04 13:09:50 +0200

    Aloitettu asiakastietojen lukeminen.

--- DotNet/DataIntegraatioDemo/DataIntegraatioDemo/Program.cs
+++ DotNet/DataIntegraatioDemo/DataIntegraatioDemo/Program.cs
@@ -6,7 +6,26 @@ namespace DataIntegraatioDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Aloitetaan asiakasdatan kopiointi.");
+
+            DataModelNorthwind.NorthwindContext northwind =
+                new DataModelNorthwind.NorthwindContext();
+
+            DataModelAdventureWorks.AdventureWorks2016CTP3Context adventure =
+                new DataModelAdventureWorks.AdventureWorks2016CTP3Context();
+
+            foreach (DataModelNorthwind.Customers customer in northwind.Customers)
+            {
+                string name = customer.ContactName;
+                string title = customer.ContactTitle;
+                string[] nameParts = name.Split(' ');
+                string first = nameParts[0];
+                string last = nameParts[1];
+
+                Console.WriteLine($"Käsitellään asiakasta: {first} {last}.");
+            }
+
+            Console.WriteLine("Asiakasdatan kopiointi on päättynyt.");
         }
     }
 }
