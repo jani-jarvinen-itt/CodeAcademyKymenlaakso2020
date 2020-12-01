0d8f5ed44cee01572d50a9e2d4b29fa2fc4d6d97
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-12-01 15:29:52 +0200

    Lisätty URL-kyselyn parametrien käsittelyesimerkki.

--- JavaScript/ExpressJsDemo/Demo.js
+++ JavaScript/ExpressJsDemo/Demo.js
@@ -3,19 +3,34 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send("<!DOCTYPE html><html> " +
-           "<head><link rel='stylesheet' href='staattiset/Testi.css'></head>" +
-           "<body><h1>JavaScript-testi</h1></body></html>")
+    res.send("<!DOCTYPE html><html> " +
+        "<head><link rel='stylesheet' href='staattiset/Testi.css'></head>" +
+        "<body><h1>JavaScript-testi</h1></body></html>")
+})
+
+app.get('/summa', (req, res) => {
+
+    const luku1 = req.query.a;
+    const luku2 = req.query.b;
+    const summa = parseInt(luku1) + parseInt(luku2);
+
+    tulos = {
+        a: luku1,
+        b: luku2,
+        summa: summa
+    }
+
+    res.json(tulos)
 })
 
 app.post('/', (req, res) => {
-    
+
     const tila = {
         onnistui: false,
         virheilmoitus: "Tietokantavirhe."
     }
     res.json(tila)
-    
+
     // res.send('Vastaus POST-pyyntöön.')
 })
 
@@ -23,5 +38,5 @@ app.post('/', (req, res) => {
 app.use('/staattiset', express.static('staattiset'))
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
