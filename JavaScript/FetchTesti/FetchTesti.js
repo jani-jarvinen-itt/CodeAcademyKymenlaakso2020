30473a70d9c7369b87162b7c833f9aee9635fb7e
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-12-02 10:16:48 +0200

    Lisätty For..Of -esimerkki fetch-datan käsittelystä.

--- JavaScript/FetchTesti/FetchTesti.js
+++ JavaScript/FetchTesti/FetchTesti.js
@@ -1,10 +1,17 @@
 const fetch = require("node-fetch");
 
 fetch('https://jsonplaceholder.typicode.com/albums')
-  .then(response => response.json())
-  .then(json => {
-      for (let index = 0; index < json.length; index++) {
-          const element = json[index];
-          console.log(element.title);
-      }
+    .then(response => response.json())
+    .then(json => {
+
+        /*
+          for (let index = 0; index < json.length; index++) {
+              const element = json[index];
+              console.log(element.title);
+          }
+        */
+
+        for (albumi of json) {
+            console.log(albumi.title);
+        }
     });
