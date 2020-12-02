d998ef6c197f66d0dd66c56d76c4d875b912dab9
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-12-02 09:56:33 +0200

    Muutettu Fetch-koodia siten, että se hakee albumit ja tulostaa niiden otsikot.

--- JavaScript/FetchTesti/FetchTesti.js
+++ JavaScript/FetchTesti/FetchTesti.js
@@ -1,5 +1,10 @@
 const fetch = require("node-fetch");
 
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+fetch('https://jsonplaceholder.typicode.com/albums')
   .then(response => response.json())
-  .then(json => console.log(json));
+  .then(json => {
+      for (let index = 0; index < json.length; index++) {
+          const element = json[index];
+          console.log(element.title);
+      }
+    });
