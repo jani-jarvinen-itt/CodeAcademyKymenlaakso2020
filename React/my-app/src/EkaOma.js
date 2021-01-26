9bd0303bc20510eb8de93792b4c839e793be6bcb
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-01-26 13:05:49 +0200

    Lisätty ensimmäinen oma React-komponentti.

---
+++ React/my-app/src/EkaOma.js
@@ -0,0 +1,10 @@
+function EkaOma() {
+    return (
+        <div>
+            <h1>Eka oma React-komponentti</h1>
+            <p>Toinen rivi tekstiä!</p>
+        </div>
+    );
+}
+
+export default EkaOma;
