8d3a56eb675b942e09ee04d5118cfc8c5a3cb687
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-01-26 14:25:26 +0200

    Lisätty Reactin props-esimerkkejä.

--- React/my-app/src/KolmasOma.js
+++ React/my-app/src/KolmasOma.js
@@ -1,8 +1,12 @@
-function KolmasOma() {
+function KolmasOma(props) {
     return (
         <div>
             <h2>Kolmas komponentti</h2>
             <p>Tämä on KolmasOma.js</p>
+            <p>Teksti on: {props.teksti}</p>
+            <p>Luku on: {props.luku}</p>
+            <p>Listan pituus: {props.lista?.length}</p>
+            <p>Henkilön nimi: {props.henkilö?.nimi}</p>
         </div>
     );
 }
--- React/my-app/src/TokaOma.js
+++ React/my-app/src/TokaOma.js
@@ -1,14 +1,21 @@
 import KolmasOma from "./KolmasOma";
 
 function TokaOma() {
+
+    const taulukko = [1, 2, 3];
+    const olio = {
+        nimi: "Jaska Jokunen",
+        kengännumero: 42
+    };
+
     return (
         <div>
             <h1>Toinen komponentti</h1>
             <p>Tämä on TokaOma.js</p>
 
-            <KolmasOma />
-            <KolmasOma />
-            <KolmasOma />
+            <KolmasOma teksti="ABCD" lista={taulukko} henkilö={olio} />
+            <KolmasOma luku={1234} />
+            <KolmasOma teksti="BCDE" luku={2345} />
         </div>
     );
 }
