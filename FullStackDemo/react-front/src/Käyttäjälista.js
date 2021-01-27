7b8cbf5a38fddf5731aec5e7ca1f762178512c63
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-01-27 10:29:02 +0200

    Lisätty käyttäjälista-komponenttiin yksinkertainen renderöinti.

--- FullStackDemo/react-front/src/Käyttäjälista.js
+++ FullStackDemo/react-front/src/Käyttäjälista.js
@@ -1,13 +1,31 @@
 import React from 'react';
 
 class Käyttäjälista extends React.Component {
-    render() {
-      return (
-        <div>
-          <h1>Tämä on Käyttäjälista-komponentti</h1>
-        </div>
-      );
-    }
-  }
 
-  export default Käyttäjälista;
+    constructor(props) {
+        super(props);
+        this.state = { käyttäjät: [] };
+
+        console.log("Käyttäjälista.constructor");
+    }
+
+    render() {
+
+        console.log("Käyttäjälista.render");
+
+        const lista = [];
+        for (let index = 0; index < this.state.käyttäjät.length; index++) {
+            const käyttäjä = this.state.käyttäjät[index];
+            lista.push(<p>Id: {käyttäjä.id}, nimi: {käyttäjä.name}</p>);
+        }
+
+        return (
+            <div>
+                <h1>Tämä on Käyttäjälista-komponentti</h1>
+                {lista}
+            </div>
+        );
+    }
+}
+
+export default Käyttäjälista;
