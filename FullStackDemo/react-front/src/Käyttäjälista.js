589dc988edd96a9b11d53c97757c11856561e82b
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-01-27 10:17:41 +0200

    Aloitettu lisäämään käyttäjälista-komponenttia.

---
+++ FullStackDemo/react-front/src/Käyttäjälista.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Käyttäjälista extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>Tämä on Käyttäjälista-komponentti</h1>
+        </div>
+      );
+    }
+  }
+
+  export default Käyttäjälista;
--- FullStackDemo/react-front/src/index.js
+++ FullStackDemo/react-front/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Käyttäjälista from './Käyttäjälista';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Käyttäjälista />
   </React.StrictMode>,
   document.getElementById('root')
 );
