b2502098d90b3c073d59db12a3ed4bcbeff31d7e
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-01-27 13:50:09 +0200

    Lisätty API-kontrolleri, joka palauttaa Customers-taulun tiedot SQL-tietokannasta.

---
+++ FullStackDemo/FullStackAspNetWebApiDemo/FullStackAspNetWebApiDemo/Controllers/CustomersController.cs
@@ -0,0 +1,21 @@
+﻿using FullStackAspNetWebApiDemo.Database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FullStackAspNetWebApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        public List<Customer> ListaaKaikki()
+        {
+            NorthwindContext konteksti = new NorthwindContext();
+            return konteksti.Customers.ToList();
+        }
+    }
+}
