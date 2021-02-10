b87970d97772c652dd3c2e260d3176dd44d90ce3
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-02-10 14:37:02 +0200

    Lisätty Cosmos DB -tietokantaesimerkki.

---
+++ DotNet/AzureCosmosDbDemo/AzureCosmosDbDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureCosmosDbDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
